541d10885498aedbec03545dc0b4143a5af120b0
Víctor Gracia Magallón <info@victorgraciaweb.com>
2025-07-27 23:08:22 +0300

    Added findProductImage unit test

--- src/files/files.controller.spec.ts
+++ src/files/files.controller.spec.ts
@@ -1,10 +1,12 @@
 import { Test, TestingModule } from "@nestjs/testing";
+import { Response } from 'express';
 import { FilesController } from "./files.controller"
 import { ConfigService } from "@nestjs/config";
 import { FilesService } from "./files.service";
 
 describe('FilesController', ()=>{
     let controller: FilesController;
+    let filesService: FilesService;
 
     beforeEach(async ()=>{
         const mockFilesService = {
@@ -30,9 +32,30 @@ describe('FilesController', ()=>{
         }).compile();
 
         controller = module.get<FilesController>(FilesController);
+        filesService = module.get<FilesService>(FilesService);
     })
 
     it('Should be defined', ()=>{
         expect(controller).toBeDefined();
+    })
+
+    describe('findProductImage', ()=>{
+        it('Should return file path when function is called', ()=>{
+            const mockResponse = {
+                sendFile: jest.fn()
+            } as unknown as Response;
+
+            const imageName = 'image.jpg';
+            const filePath = `url-file/${imageName}`;
+
+            jest.spyOn(filesService, 'getStaticProductImage').mockReturnValue(filePath);
+
+            controller.findProductImage(mockResponse, imageName);
+            expect(mockResponse.sendFile).toHaveBeenCalledWith(filePath);
+        })
+    })
+
+    describe('uploadProductImage', ()=>{
+        
     })
 })
