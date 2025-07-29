57c0231c1429d2aa36f2f39f33dcaa2679e31d77
Víctor Gracia Magallón <info@victorgraciaweb.com>
2025-07-29 13:24:09 +0300

    Added e2e test on login

--- test/e2e/auth/login.e2e-spec.ts
+++ test/e2e/auth/login.e2e-spec.ts
@@ -1,12 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
-import * as request from 'supertest';
 import { AppModule } from './../../../src/app.module';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import * as request from 'supertest';
+import { User } from '../../../src/auth/entities/user.entity';
+
+const testingUser = {
+  email: 'testing.user@gmail.com', 
+  password: 'Abc123',
+  fullName: 'Test User'
+} as User
+
+const testingAdmin = {
+  email: 'testing.admin@gmail.com', 
+  password: 'Abc123',
+  fullName: 'Test Admin'
+} as User
 
 describe('Auth - login', () => {
   let app: INestApplication;
+  let userRepository: Repository<User>;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -21,11 +37,35 @@ describe('Auth - login', () => {
     );
 
     await app.init();
+
+    userRepository = app.get<Repository<User>>(getRepositoryToken(User));
+
+    // Removed users if exist
+    userRepository.delete({email: testingUser.email});
+    userRepository.delete({email: testingAdmin.email});
+
+    // Added user for testing
+    await request(app.getHttpServer())
+    .post('/auth/register')
+    .send(testingUser);
+
+    const result = await request(app.getHttpServer())
+    .post('/auth/register')
+    .send(testingAdmin);
+
+    // Updated testingAdmin with properly roles ('user') by default
+    userRepository.update(
+      { email: testingAdmin.email },
+      { roles: ['admin'] }
+    )
   });
 
-  afterAll(()=>{
-    app.close();
-  })
+  afterAll(async () => {
+    await userRepository.delete({ email: testingUser.email });
+    await userRepository.delete({ email: testingAdmin.email });
+
+    await app.close();
+  });
 
   it('/auth/login (POST) - should throw 400 if no body', async () => {
     const response = await request(app.getHttpServer()).post('/auth/login');
@@ -51,7 +91,7 @@ describe('Auth - login', () => {
     .send(
       {
         "email": "no-exist@gmail.com",
-        "password": "123456abcABC"
+        "password": testingUser.password
       }
     );
 
@@ -64,8 +104,8 @@ describe('Auth - login', () => {
     .post('/auth/login')
     .send(
       {
-        "email": "test1@google.com",
-        "password": "123456abcABC"
+        "email": testingUser.email,
+        "password": 'Abc123-ERROR'
       }
     );
 
@@ -73,13 +113,13 @@ describe('Auth - login', () => {
     expect(response.body.message).toEqual('Credentials are not valid (password)')
   });
 
-  it('/auth/login (POST) - valid credentials', async () => {
+  it('/auth/login (POST) - valid credentials (user rol)', async () => {
     const response = await request(app.getHttpServer())
     .post('/auth/login')
     .send(
       {
-        "email": "test1@google.com",
-        "password": "Abc123"
+        "email": testingUser.email,
+        "password": testingUser.password
       }
     );
 
@@ -88,15 +128,33 @@ describe('Auth - login', () => {
     expect(response.status).toEqual(201);
     expect(user).toBeDefined();
     expect(user).toHaveProperty('id');
-    expect(user).toHaveProperty('email', 'test1@google.com');
-    expect(user).toHaveProperty('fullName', 'Test One');
+    expect(user).toHaveProperty('email');
+    expect(user).toHaveProperty('fullName');
     expect(user).toHaveProperty('isActive', true);
     expect(user).toHaveProperty('roles');
     expect(Array.isArray(user.roles)).toBe(true);
-    expect(user.roles).toContain('admin');
+    expect(user.roles).toContain('user');
 
     expect(token).toBeDefined();
     expect(typeof token).toBe('string');
     expect(token.split('.').length).toBe(3);
+  });
+
+  it('/auth/login (POST) - valid credentials (admin rol)', async () => {
+    const response = await request(app.getHttpServer())
+    .post('/auth/login')
+    .send(
+      {
+        "email": testingAdmin.email,
+        "password": testingAdmin.password
+      }
+    );
+
+    const { user, token } = response.body;
+
+    expect(response.status).toEqual(201);
+    expect(user).toHaveProperty('roles');
+    expect(Array.isArray(user.roles)).toBe(true);
+    expect(user.roles).toContain('admin');
   });
 });
